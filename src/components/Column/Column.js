30d385f96e6b089eb7a626ee77d8aede718d4311
PhucLH1 <phuclh1@splus-software.com.vn>
2021-09-05 18:00:45 +0700

    add new card

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,13 +1,13 @@
 import Card from 'components/Card/Card'
-import React, { useEffect, useState, useCallback } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Column.scss'
 import { mapOrder } from 'utilities/sorts'
 import { Container, Draggable } from 'react-smooth-dnd'
-import { Dropdown, Form } from 'react-bootstrap'
+import { Button, Dropdown, Form } from 'react-bootstrap'
 import Modals from 'components/Common/Modals'
 import { MODAL_ACTION_CONFIRM } from 'utilities/constants'
 import { saveColumnTitleByEnter, selectAllInlineText } from 'utilities/constantEditTable'
-
+import { cloneDeep } from 'lodash'
 
 export default function Column(props) {
 
@@ -19,11 +19,28 @@ export default function Column(props) {
 
   const [columnTitle, setColumnTitle] = useState('')
 
-  const handleChangeTitle = useCallback((e) => {
-    setColumnTitle(e.target.value)
-  }, [])
+  const [openNewCard, setOpenNewCard] = useState(false)
 
-  useEffect( () => {
+  const [newCardTitles, setNewCardTitles] = useState('')
+
+  const onNewCardTile = (e) => setNewCardTitles(e.target.value)
+
+  const inputRef = useRef(null)
+
+  const toggleOpenNewCard = () => {
+    setOpenNewCard(!openNewCard)
+  }
+
+  const handleChangeTitle = (e) => setColumnTitle(e.target.value)
+
+  useEffect(() => {
+    if (inputRef && inputRef.current) {
+      inputRef.current.focus()
+      inputRef.current.select()
+    }
+  }, [openNewCard])
+
+  useEffect(() => {
     setColumnTitle(column.title)
   }, [column.title]) //khi nào title thay đổi thì chạy
 
@@ -49,6 +66,28 @@ export default function Column(props) {
     onUpdateColumn(newColumn)
   }
 
+  const addNewCard = () => {
+    if (!newCardTitles) {
+      inputRef.current.focus()
+    }
+    else {
+      const newCardAdd = {
+        id: Math.random().toString(36).substr(2, 5), //random characters
+        boardId: column.boardId,
+        title: newCardTitles.trim(),
+        columnId: column.id,
+        cover: null
+      }
+      // console.log(newCardAdd)
+      let newColumn = cloneDeep(column)
+      newColumn.cards.push(newCardAdd)
+      newColumn.cardOrder.push(newCardAdd.id)
+      onUpdateColumn(newColumn)
+      setNewCardTitles('')
+      toggleOpenNewCard()
+    }
+  }
+
   return (
     <div className="column">
       <header className="header column-drag-handle">
@@ -60,11 +99,11 @@ export default function Column(props) {
             className="tuanvu-edit-table shadow-none"
             value={columnTitle}
             spellCheck="false"
-            onClick={selectAllInlineText}
-            onChange={handleChangeTitle}
-            onBlur={handleChangeTitleBlur}
-            onKeyDown={saveColumnTitleByEnter}
-            onMouseDown={e => e.preventDefault()}
+            onClick={selectAllInlineText} // click value
+            onChange={handleChangeTitle} // thay đổi title
+            onBlur={handleChangeTitleBlur}// nhấn bên ngoài để thay đổi title
+            onKeyDown={saveColumnTitleByEnter} // nhấn ecter để đổi tên title
+            onMouseDown={e => e.preventDefault()} // lần chạm vào để di chuyển column ko thay đổi title
           />
         </div>
         <div className="column-dropdowns">
@@ -104,11 +143,43 @@ export default function Column(props) {
             ))
           }
         </Container>
+        {
+          openNewCard && <div className="add-new-card">
+            <Form.Control
+              ref={inputRef}
+              as="textarea"
+              rows="3"
+              size="small"
+              placeholder="Add new card"
+              className="textarea-new-card shadow-none"
+              value={newCardTitles}
+              onChange={onNewCardTile}
+              onKeyDown={
+                event => (event.key === 'Enter') && addNewCard()
+              }
+            />
+          </div>
+        }
       </div>
       <footer>
-        <div className='footer-actions'>
-          <i className="fa fa-plus icon" /> Add another card
-        </div>
+        {
+          openNewCard && <div className="add-new-card-button">
+            <Button
+              onClick={addNewCard}
+              variant='success' size='sm' className="shadow-none btn">Add card</Button>
+            <span className="cancel-icon">
+              <i
+                onClick={toggleOpenNewCard}
+                className="fa fa-trash icon" />
+            </span>
+          </div>
+        }
+        {
+          !openNewCard &&
+          <div onClick={toggleOpenNewCard} className='footer-actions'>
+            <i className="fa fa-plus icon" /> Add another card
+          </div>
+        }
       </footer>
       <Modals
         show={showConfirmModal}
